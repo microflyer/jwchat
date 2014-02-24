760f96006e0700b6b7ae9c918aa970095cc1b242
microflyer <microflyer@gmail.com>
2014-02-24 14:44:59 +0800

    optimized the directive so the modal html doesn't need to be inserted to a specified place

--- app/js/directives.js
+++ app/js/directives.js
@@ -9,44 +9,38 @@ directive('assignmentModal', ['$compile', '$http', '$templateCache',
         return {
             restrict: 'A',
             scope: {
-                assignment: '='
+                assignment : '='
             },
-            //templateUrl: 'partials/modal-content.html',
             replace: false,
             link: function (scope, elem, attr) {
+
+                var $modalElement;
 
                 $http.get('partials/modal-content.html', {
                     cache: $templateCache
                 })
                     .success(function (htmlToAdd) {
-                        if (!$(elem).attr('compiled')) {
-                            $(elem).attr('compiled', "true");
+                        if (!elem.attr('compiled')) {
+                            elem.attr('compiled', "true");
 
-                            $(elem).on('click', function () {
-                                $(elem).find('.modal').modal('show');
-                                $(elem).off('click');
-                            });
-
-                            var htmlContent = $(elem).html();
-                            var modalHtmlBefore = '<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
+                            var modalHtmlBefore = '<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="assignmentModal" aria-hidden="true">';
                             modalHtmlBefore += '<div class="modal-dialog"><div class="modal-content"><div class="modal-body">';
                             var modalHtmlAfter = '</div></div></div></div>';
 
-                            $(elem).html(htmlContent + modalHtmlBefore + htmlToAdd + modalHtmlAfter);
+                            var modalHtml = modalHtmlBefore + htmlToAdd + modalHtmlAfter;
+                            $modalElement = $compile(modalHtml)(scope);
+                            $modalElement.find('.modal-dialog').css("width", "850px");
+                            $modalElement.find('.modal-body').css("height", "800px");
 
-                            $(elem).find('.modal-dialog').css("width", "850px");
-                            $(elem).find('.modal-body').css("height", "800px");
-
-                            $(elem).find('.modal').on('hidden.bs.modal', function (e) {
-                                $(elem).on('click', function () {
-                                    $(elem).find('.modal').modal('show');
-                                    $(elem).off('click');
-                                });
+                            elem.on('click', function () {
+                                $modalElement.modal('show');
                             });
-
-                            $compile(elem)(scope);
                         }
                     });
+
+                scope.close = function() {
+                    $modalElement.modal('hide');
+                }
             }
         };
     }
